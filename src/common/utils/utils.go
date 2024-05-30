12813914d92bd781a440e247aa6dddc84a42afac
MIRIAM-SLOV <mirislovat@gmail.com>
2024-05-30 13:34:47 +0300

    skip resource or all resource in file by comment (yaml, tf)

--- src/common/utils/utils.go
+++ src/common/utils/utils.go
@@ -17,6 +17,15 @@ import (
 
 // RemoveGcpInvalidChars Source of regex: https://cloud.google.com/compute/docs/labeling-resources
 var RemoveGcpInvalidChars = regexp.MustCompile(`[^\p{Ll}\p{Lo}\p{N}_-]`)
+var SkipResByComment = make ([]string, 0) 
+
+
+func AppendSkippedRunner( skippedResources *[]string){
+
+	*skippedResources = append(*skippedResources,SkipResByComment...)
+	SkipResByComment = SkipResByComment[:0]
+	
+     }
 
 func InSlice[T comparable](elems []T, v T) bool {
 	for _, s := range elems {
